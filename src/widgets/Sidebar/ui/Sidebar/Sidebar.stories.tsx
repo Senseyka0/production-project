c5fe82f3fdc22a37bfbb14dd283778a88738dbe3
Mikhail Rypta <mikhailrypta@Mikhails-MacBook-Pro.local>
2022-11-29 03:12:53 +0200

    add Sidebar Auth/Not Auth storypage

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { StoreDecorator } from "shared/config/storybook";
 
 import { Sidebar } from "./Sidebar";
 
@@ -11,3 +12,19 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 
 export const Standard = Template.bind({});
 Standard.args = {};
+
+export const Auth = Template.bind({});
+Auth.args = {};
+Auth.decorators = [
+	StoreDecorator({
+		user: { isAuth: true, authData: { id: "1", username: "senseyka" } },
+	}),
+];
+
+export const NotAuth = Template.bind({});
+NotAuth.args = {};
+NotAuth.decorators = [
+	StoreDecorator({
+		user: { isAuth: false },
+	}),
+];
